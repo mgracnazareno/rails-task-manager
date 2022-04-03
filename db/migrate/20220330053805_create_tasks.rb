9c44b323131ec212dff723bfbf2550c78415d25e
Mary Grace Nazareno <mgracenazareno@gmail.com>
2022-04-03 02:34:13 -0400

    rails index complete

--- db/migrate/20220330053805_create_tasks.rb
+++ db/migrate/20220330053805_create_tasks.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string, :title
-      t.text, :details
-      t.false :completed
+      t.string :title
+      t.text :details
+      t.boolean :completed, false
 
       t.timestamps
     end
