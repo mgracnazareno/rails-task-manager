02d102bae4250507d473cdf074d82640a7533136
Mary Grace Nazareno <mgracenazareno@gmail.com>
2022-04-03 22:19:08 -0400

    rails action new & create + view complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,28 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = 'Object successfully created'
+      redirect_to @task
+    else
+      flash[:error] = 'Something went wrong'
+      render 'new'
+    end
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
