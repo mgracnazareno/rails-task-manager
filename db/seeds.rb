1d5c76e6915b7418955f1b8798079140e1f0cb64
Mary Grace Nazareno <mgracenazareno@gmail.com>
2022-04-07 10:51:47 -0400

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database'
+Task.destroy_all
+
+puts 'Creating Tasks...'
+final_project = { title: 'Rails Project', details: 'Deadline is on the 14th of April', completed: true }
+tire_change = { title: 'Summer Tire Change', details: 'Set an appointment to the dealers', completed: true }
+
+[final_project, tire_change].each do |attributes|
+  task = Task.create!(attributes)
+  puts "Created #{task.title}"
+end
+puts 'Finished'
